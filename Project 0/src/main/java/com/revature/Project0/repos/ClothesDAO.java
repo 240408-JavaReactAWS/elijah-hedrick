37c4e2e5d7090e772240f6a739a248ed7f60cf07
EliHedrick <eli.hedrickk@gmail.com>
2024-04-11 16:59:45 -0400

    Starting project

---
+++ Project 0/src/main/java/com/revature/Project0/repos/ClothesDAO.java
@@ -0,0 +1,10 @@
+package com.revature.Project0.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.revature.Project0.models.Clothes;
+
+public interface ClothesDAO {
+
+
+}
