8a12872ad86f2a07f381c9b99044f040626d0fa3
EliHedrick <eli.hedrickk@gmail.com>
2024-04-15 17:38:45 -0400

    Vendor get and delete methods

--- Project 0/src/main/java/com/revature/Project0/controllers/VendorController.java
+++ Project 0/src/main/java/com/revature/Project0/controllers/VendorController.java
@@ -1,9 +1,18 @@
 package com.revature.Project0.controllers;
 
+import com.revature.Project0.exceptions.GarmentNotFoundException;
+import com.revature.Project0.exceptions.VendorNotFoundException;
+import com.revature.Project0.models.Garment;
 import com.revature.Project0.models.Vendor;
 import com.revature.Project0.services.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/vendors")
@@ -24,5 +33,31 @@ public class VendorController {
     @GetMapping
     public Vendor loginVendorHandler(@RequestBody Vendor vendor) {
         return vendorService.loginAsVendor(vendor);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Vendor> getVendorByIdHandler(@PathVariable int id) {
+        Vendor vendor;
+        try {
+            vendor = vendorService.findVendorById(id);
+        } catch (VendorNotFoundException e) {
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        return new ResponseEntity<>(vendor, OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<List<Vendor>> deleteVendorByIdHandler(@PathVariable int id) {
+        try {
+            vendorService.deleteVendorById(id);
+        } catch (GarmentNotFoundException e) {
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        return new ResponseEntity<>(this.getAllVendorsHandler(), OK);
+    }
+
+    @GetMapping("/all")
+    public List<Vendor> getAllVendorsHandler() {
+        return vendorService.getAllVendors();
     }
 }
--- Project 0/src/main/java/com/revature/Project0/services/VendorService.java
+++ Project 0/src/main/java/com/revature/Project0/services/VendorService.java
@@ -1,12 +1,16 @@
 package com.revature.Project0.services;
 
 
+
 import com.revature.Project0.exceptions.GarmentNotFoundException;
 import com.revature.Project0.exceptions.VendorNotFoundException;
+import com.revature.Project0.models.Garment;
 import com.revature.Project0.models.Vendor;
 import com.revature.Project0.repos.VendorDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VendorService {
@@ -28,4 +32,17 @@ public class VendorService {
                 -> new VendorNotFoundException("!! Incorrect Vendor Name or Password !!"));
     }
 
+    public void deleteVendorById(int id) {
+        vendorDAO.delete(this.findVendorById(id));
+        return; // to check if the item is successfully deleted
+    }
+
+    public Vendor findVendorById (int id) {
+        return vendorDAO.findById(id).orElseThrow(() -> new VendorNotFoundException("No Vendor found with id: " + id));
+    }
+
+    public List<Vendor> getAllVendors(){
+        // Now we call upon our DAO layer to get all of our users
+        return vendorDAO.findAll();
+    }
 }
